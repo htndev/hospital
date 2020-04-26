94df6f107b6b02d82147a00d8010392d7b6fc551
htnpro <he4then.mail@gmail.com>
2020-04-26 18:56:44 +0300

    chore: doctors loader

--- src/views/Doctors.vue
+++ src/views/Doctors.vue
@@ -1,50 +1,52 @@
 <template>
   <div>
     <h1>Врачи</h1>
-    <template v-if="doctors.length">
-      <v-row
-          no-gutters
-          justify="center"
-      >
-        <v-col
+    <template v-if="doctorsLoading">
+    <div class="text-center">
+      <v-progress-circular indeterminate color="primary" />
+    </div>
+    </template>
+    <template v-else>
+      <template v-if="doctors.length">
+        <v-row no-gutters justify="center">
+          <v-col
             cols="3"
             v-for="(doctor, i) in doctors"
             :key="i"
             class="mr-3 mb-3"
-        >
-          <doctor
-              :doctor="doctor"
-          />
-        </v-col>
-      </v-row>
+          >
+            <doctor :doctor="doctor" />
+          </v-col>
+        </v-row>
+      </template>
+      <h3 v-else>Увы, врачей не найдено.</h3>
     </template>
-    <h3 v-else>Увы, врачей не найдено.</h3>
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  // @ts-ignore
-  import Doctor from '@/components/Doctor.vue';
-  import { DoctorItem } from '@/common/types';
+import { Component, Vue } from 'vue-property-decorator';
+// @ts-ignore
+import Doctor from '@/components/Doctor.vue';
+import { DoctorItem } from '@/common/types';
 
-  @Component({
-    name: 'Doctors',
-    components: { Doctor }
-  })
-  export default class Doctors extends Vue {
-    doctors: DoctorItem[] = [];
-
-    async created() {
-      const doctorsRequest = await this.$api.doctor.get('');
-      if(doctorsRequest.status >= 400) {
-        return;
-      }
-      this.doctors = doctorsRequest.data;
+@Component({
+  name: 'Doctors',
+  components: { Doctor }
+})
+export default class Doctors extends Vue {
+  doctors: DoctorItem[] = [];
+  doctorsLoading = false;
+  async created() {
+    this.doctorsLoading = true;
+    const doctorsRequest = await this.$api.doctor.get('');
+    if (doctorsRequest.status >= 400) {
+      return;
     }
+    this.doctors = doctorsRequest.data;
+    this.doctorsLoading = false;
   }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
