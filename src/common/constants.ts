1a605c7c1ee3395a4d1224e0bae4bb29c8ed8abc
htnpro <he4then.mail@gmail.com>
2020-05-05 21:24:54 +0300

    chore: API URL

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
-// export const API_URL = 'https://htn-hospital-backend.herokuapp.com';
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://htn-hospital-backend.herokuapp.com';
+// export const API_URL = 'http://localhost:3000';
 
 export const DEFAULT_DOCTOR_IMAGE = 'DEFAULT_DOCTOR.jpg';
 
